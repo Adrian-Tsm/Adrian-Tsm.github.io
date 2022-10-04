92f408a00598b093f91acd054247ce3dc638fe2e
Adrian-Tsm <102124748+Adrian-Tsm@users.noreply.github.com>
2022-10-04 08:23:24 -0700

    Add files via upload

---
+++ POSTTEST 3/script.js
@@ -0,0 +1,12 @@
+<head>
+    <script>
+        alert("Selamat Datang");
+    </script>
+</head>
+
+const Driann Cell = document.getElementsByClassName('nama');
+judul.style.color = 'blue';
+
+hdcategory.addEventListener("mouseleave", function(){
+    hdcategory.style.color = ["#ffa8a8"]
+});
